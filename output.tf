2e02a2a6a23b600aaed9037ebea04f5c48b518e6
Noel Colon <ncolon@us.ibm.com>
2019-07-30 14:26:25 -0400

    prepare for module dependencies

--- output.tf
+++ output.tf
@@ -8,3 +8,9 @@ resource "random_id" "completed" {
         "null_resource.create_cluster_admin",
     ]
 }
+
+resource "null_resource" "dependency" {
+  triggers = {
+    all_dependencies = "${join(",", var.dependencies)}"
+  }
+}
